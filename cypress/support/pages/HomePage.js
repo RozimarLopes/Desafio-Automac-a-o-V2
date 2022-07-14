3667298ad80252f1b72b6c7be10c6fb8f2ae4240
RozimarLopes <109172736+RozimarLopes@users.noreply.github.com>
2022-07-14 15:59:16 -0300

    Ajuste HomePage

--- cypress/support/pages/HomePage.js
+++ cypress/support/pages/HomePage.js
@@ -36,11 +36,11 @@ class HomePage {
     Secao(tipo) {
         switch (tipo) {
             case 'News':
-                cy.get(this.ValidaTituloNews).should('contain', 'New injector region: South America')
+                cy.get(this.ValidaTituloNews).should('contain', 'New injector region: Mumbai, India')
                 cy.get(this.News2).click()
-                cy.get(this.ValidaTituloNews).should('contain', 'Gatling Academy Modules now available in Java')
+                cy.get(this.ValidaTituloNews).should('contain', 'Gatling Academy Module 3: load test a RESTful API')
                 cy.get(this.News3).click()
-                cy.get(this.ValidaTituloNews).should('contain', '5 Steps to…')
+                cy.get(this.ValidaTituloNews).should('contain', 'Introducing Gatling 3.8')
                 break;
             case 'Depoimento':
                 cy.get(this.Depoimento2).click()
